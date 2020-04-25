3445d049d209c80de2a2e00785423a6d30b93cbc
aalarconbojorque <aa.alarcon10@gmail.com>
2020-04-25 08:59:32 -0700

    added more functions

--- graph.cpp
+++ graph.cpp
@@ -27,7 +27,7 @@ graph<T>::~graph(){}
 
 template<class T>
 bool graph<T>::isEmpty(){
-	return numberOfEdges == 0;
+	return numberOfVertices == 0;
 }
 
 template<class T>
@@ -42,21 +42,59 @@ int graph<T>::getNumEdges(){
 
 template<class T>
 bool graph<T>::checkEdge(T start, T end){
-	if(isEmpty()){
-		return false;
+	
+	if(!isEmpty())
+	{
+		int startIndex = getVertexPos(start);
+		int endIndex = getVertexPos(end);
+
+		//Value is > -1 so the vertices are in the array
+		if(startIndex >= 0 && endIndex >= 0 && edgeArray[startIndex][endIndex] > 0)
+			return true;		
 	}
-	//else if(){}
+
 	return false;
 }
 
 template<class T>
 bool graph<T>::addEdge(T start, T end, int weight){
 	
+	if(!isEmpty())
+	{
+		int startIndex = getVertexPos(start);
+		int endIndex = getVertexPos(end);
+
+		//Value is > -1 so the vertices are in the array
+		if(startIndex >= 0 && endIndex >= 0)
+		{
+			edgeArray[startIndex][endIndex] = weight;
+			numberOfEdges++;
+			return true;
+		}
+	}
+
+	return false;
+	
 }
 
 template<class T>
 bool graph<T>::removeEdge(T start, T end){
-	
+	if(!isEmpty())
+	{
+		int startIndex = getVertexPos(start);
+		int endIndex = getVertexPos(end);
+
+		//Value is > -1 so the vertices are in the array
+		if(startIndex >= 0 && endIndex >= 0)
+		{
+			edgeArray[startIndex][endIndex] = 0;
+			numberOfEdges--;
+			return true;
+		}
+
+	}
+
+	return false;
 }
 
 template<class T>
@@ -78,17 +116,41 @@ bool graph<T>::removeVertex(T value){
 
 template<class T>
 int graph<T>::getEdgeWeight(T start, T end){
-	
+	if(!isEmpty())
+	{
+		int startIndex = getVertexPos(start);
+		int endIndex = getVertexPos(end);
+
+		//Value is > -1 so the vertices are in the array
+		if(startIndex >= 0 && endIndex >= 0)
+			return edgeArray[startIndex][endIndex];
+	}
+
+	return -1;
 }
 
 template<class T>
-int graph<T>::getVertexPos(T value){
-	
+int graph<T>::getVertexPos(T value){	
+	if(!isEmpty())
+	{
+		//Search vertex array to get position
+		for (int i = 0; i < numberOfVertices ; i++)
+		{
+			if(vertexArray[i] == value)
+			return i;
+		}
+	}
+
+	return -1;
 }
 
 template<class T>
 T graph<T>::getVertexVal(int pos){
+	if(!isEmpty() && pos < numberOfVertices)
+		return vertexArray[pos];
 	
+
+	return false;
 }
 
 template<class T>
